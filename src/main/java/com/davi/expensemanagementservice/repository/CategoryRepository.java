be32e20f5e7d745026a020e9d4f8d9c8b1c0b284
Davi Siqueira <davisvsiqueira@gmail.com>
2024-11-06 18:15:58 -0300

    feat: add category repository

---
+++ src/main/java/com/davi/expensemanagementservice/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.davi.expensemanagementservice.repository;
+
+import com.davi.expensemanagementservice.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
