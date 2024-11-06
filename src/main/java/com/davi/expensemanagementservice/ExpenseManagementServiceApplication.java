b1df12b028bb2a9724eacbcdcb4c8a20dcee3a7b
Davi Siqueira <davisvsiqueira@gmail.com>
2024-11-06 10:51:56 -0300

    chore: initial commit

---
+++ src/main/java/com/davi/expensemanagementservice/ExpenseManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.davi.expensemanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseManagementServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/davi/expensemanagementservice/ExpenseManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davi.expensemanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
