40a0363f643e4b5760cd95b24412765c649b47e9
Davi Siqueira <davisvsiqueira@gmail.com>
2024-11-06 18:33:59 -0300

    feat: add @Getter annotation to BaseEntity

--- src/main/java/com/davi/expensemanagementservice/model/BaseEntity.java
+++ src/main/java/com/davi/expensemanagementservice/model/BaseEntity.java
@@ -2,17 +2,18 @@ package com.davi.expensemanagementservice.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
+@Getter
 @MappedSuperclass
 public abstract class BaseEntity {
 
     @CreationTimestamp
     @Column(updatable = false)
-
     private LocalDateTime createdAt;
 
     @UpdateTimestamp
