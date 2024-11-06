0a34f7a160adff67fa22a5ac67fd690386f22e20
Davi Siqueira <davisvsiqueira@gmail.com>
2024-11-06 18:16:32 -0300

    feat: add custom exception handler

---
+++ src/main/java/com/davi/expensemanagementservice/config/exception/CustomExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.davi.expensemanagementservice.config.exception;
+
+import com.davi.expensemanagementservice.exception.CategoryNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class CustomExceptionHandler {
+    private Map<String, String> response(String errorMessage) {
+        var map = new HashMap<String, String>();
+        map.put("error", errorMessage);
+        return map;
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<Map<String, String>> handleCategoryNotFoundException(CategoryNotFoundException e) {
+        return new ResponseEntity<>(response(e.getMessage()), HttpStatus.NOT_FOUND);
+    }
+}
