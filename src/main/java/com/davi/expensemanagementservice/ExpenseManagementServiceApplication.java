6dbede6ea4b5fac4929a277f0dedf7efe5804f05
Davi Siqueira <davisvsiqueira@gmail.com>
2024-11-12 18:58:49 -0300

    feat: declare PropertySource annotation on main file

--- src/main/java/com/davi/expensemanagementservice/ExpenseManagementServiceApplication.java
+++ src/main/java/com/davi/expensemanagementservice/ExpenseManagementServiceApplication.java
@@ -2,8 +2,10 @@ package com.davi.expensemanagementservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("file:${user.dir}/.env")
 public class ExpenseManagementServiceApplication {
 
     public static void main(String[] args) {
