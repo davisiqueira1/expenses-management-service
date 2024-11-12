d33758af25b43352e937c90193b85b0ffa99f1d2
Davi Siqueira <davisvsiqueira@gmail.com>
2024-11-12 18:33:05 -0300

    feat: add findByUsername method to app user repository

--- src/main/java/com/davi/expensemanagementservice/repository/AppUserRepository.java
+++ src/main/java/com/davi/expensemanagementservice/repository/AppUserRepository.java
@@ -2,9 +2,12 @@ package com.davi.expensemanagementservice.repository;
 
 import com.davi.expensemanagementservice.model.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
     AppUser findByEmail(String email);
+
+    UserDetails findByUsername(String username);
 }
