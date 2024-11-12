2ba375f0cd0834260d34435f02ca8ed413f35b2f
Davi Siqueira <davisvsiqueira@gmail.com>
2024-11-12 18:28:50 -0300

    feat: implement UserDetails interface on AppUser

--- src/main/java/com/davi/expensemanagementservice/model/AppUser.java
+++ src/main/java/com/davi/expensemanagementservice/model/AppUser.java
@@ -1,5 +1,6 @@
 package com.davi.expensemanagementservice.model;
 
+import com.davi.expensemanagementservice.enums.UserRole;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
@@ -7,7 +8,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 @EqualsAndHashCode(callSuper = true, of = "userId")
@@ -17,10 +22,22 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "userId")
-public class AppUser extends BaseEntity {
+public class AppUser extends BaseEntity implements UserDetails {
+    public AppUser(String username, String name, String password, String email, UserRole role, List<Expense> expenses) {
+        this.username = username;
+        this.name = name;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+        this.expenses = expenses;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
+
+    @Column(unique = true)
+    private String username;
 
     private String name;
     private String password;
@@ -28,6 +45,23 @@ public class AppUser extends BaseEntity {
     @Column(unique = true)
     private String email;
 
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
     @OneToMany(mappedBy = "user")
     private List<Expense> expenses;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this.role == UserRole.ADMIN) {
+            return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        }
+
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
 }
