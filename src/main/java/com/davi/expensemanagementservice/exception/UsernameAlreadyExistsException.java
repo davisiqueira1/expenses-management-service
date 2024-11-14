2757231945524eacb0d514d4d430ab0a2fa8d52d
Davi Siqueira <davisvsiqueira@gmail.com>
2024-11-14 14:06:24 -0300

    feat: add username already exists custom exception

---
+++ src/main/java/com/davi/expensemanagementservice/exception/UsernameAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.davi.expensemanagementservice.exception;
+
+public class UsernameAlreadyExistsException extends RuntimeException {
+    public UsernameAlreadyExistsException(String message) {
+        super(message);
+    }
+}
